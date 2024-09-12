4979c51d63342f5eed5d3110c44a66db68e5db69
colazz64 <116669393+colazz64@users.noreply.github.com>
2024-09-12 17:43:02 -0400

    Bug fixed!
    Game over text present correctly

--- 2D-Project/Assets/Scripts/GameManager.cs
+++ 2D-Project/Assets/Scripts/GameManager.cs
@@ -22,7 +22,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+         gameOverText.SetActive(false);
     }
 
     // Update is called once per frame
